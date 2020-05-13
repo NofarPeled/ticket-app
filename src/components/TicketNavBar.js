320548c0c1b4d24963bf297482e5e41e0c2c3379
NofarPeled <Nofarpeled4@gmail.com>
2020-05-13 18:50:52 +0300

    rededign the list, added navbar event listener

--- src/components/TicketNavBar.js
+++ src/components/TicketNavBar.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import SortTicket from './SortTicket';
 
 const TicketNavBar = props => {
 
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(()=> {
+        window.addEventListener('resize', () => {
+            setWidth(window.innerWidth);
+        });
+        return () => {
+            window.removeEventListener('resize', () => {});
+        };
+    });
     return (
         <nav className = 'ticket-nav-bar'>
             <SortTicket onSetSortBy = { props.onSetSortBy }/>
-            <Link className = 'add-ticket-link' to = { process.env.PUBLIC_URL + '/ticket/edit' }><i className='fas fa-plus'></i></Link>
+            <Link className = 'add-ticket-link' to = { process.env.PUBLIC_URL + '/ticket/edit' }>
+                { width > 750 ? 'New Ticket' : <i className='fas fa-plus'></i> }
+            </Link>
         </nav>
     )
 }
